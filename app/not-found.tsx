17074fd0366a9d471f3c67338324e19854447ac4
zer0 <osom8979@gmail.com>
2024-01-12 11:10:31 +0900

    Fix not-found page's error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,12 +2,16 @@ import Link from 'next/link';
 
 export default async function NotFound() {
   return (
-    <div className="min-h-screen bg-base-200">
-      <main>
-        <h2>Not Found</h2>
-        <p>Could not find requested resource</p>
-        <Link href="/">Return Home</Link>
-      </main>
-    </div>
+    <html>
+      <body>
+        <main className="min-h-screen bg-base-200">
+          <div className="container">
+            <h2>Not Found</h2>
+            <p>Could not find requested resource</p>
+            <Link href="/">Return Home</Link>
+          </div>
+        </main>
+      </body>
+    </html>
   );
 }
