391bd1640680e9ac2cced23ae7fb37fb71e870bb
zer0 <osom8979@gmail.com>
2024-01-13 17:18:52 +0900

    Create component: NotFoundCard

---
+++ app/components/NotFoundCard.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React from 'react';
+import GoBackButton from '@/app/components/GoBackButton';
+import useTranslation from '@/app/libs/i18n/client';
+
+export default function NotFound() {
+  const {t} = useTranslation(undefined, 'not-found');
+  return (
+    <div className="card bg-base-100 shadow-lg">
+      <div className="card-body items-center text-center">
+        <h1 className="font-light text-4xl">404</h1>
+
+        <h2 className="card-title my-2">{t('title')}</h2>
+
+        <p>{t('details')}</p>
+
+        <div className="card-actions mt-6">
+          <GoBackButton className="btn btn-sm btn-primary">
+            <span>{t('back')}</span>
+          </GoBackButton>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,35 +1,19 @@
 import Link from 'next/link';
 import React from 'react';
-import GoBackButton from '@/app/components/GoBackButton';
 import Logo from '@/app/components/Logo';
-import useTranslation from '@/app/libs/i18n/server';
+import NotFoundCard from '@/app/components/NotFoundCard';
 
 export default async function NotFound() {
-  const {t} = await useTranslation(undefined, 'not-found');
   return (
     <html>
-      <body className="bg-base-200 h-screen">
+      <body className="h-screen">
         <main className="m-auto h-full">
           <div className="flex flex-col justify-center items-center h-full space-y-8">
-            <figure>
-              <Link href="/">
-                <Logo height="1.2em" />
-              </Link>
-            </figure>
+            <Link href="/">
+              <Logo height="1.2em" />
+            </Link>
 
-            <div className="card bg-base-100 shadow-lg">
-              <div className="card-body items-center text-center">
-                <h2 className="card-title my-2">{t('title')}</h2>
-
-                <p>{t('details')}</p>
-
-                <div className="card-actions mt-8">
-                  <GoBackButton className="btn btn-sm btn-primary rounded">
-                    <span>{t('back')}</span>
-                  </GoBackButton>
-                </div>
-              </div>
-            </div>
+            <NotFoundCard />
           </div>
         </main>
       </body>
