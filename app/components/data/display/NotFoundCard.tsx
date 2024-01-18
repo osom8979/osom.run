b0871ffbe9a1c62220ff542fbb9ce4dd03e183ee
zer0 <osom8979@gmail.com>
2024-01-18 10:37:33 +0900

    Move locale file: not-found -> components/data/display/NotFoundCard

--- app/components/data/display/NotFoundCard.tsx
+++ app/components/data/display/NotFoundCard.tsx
@@ -5,8 +5,12 @@ import GoBackButton from '@/app/components/data/input/GoBackButton';
 import TablerError404 from '@/app/icons/tabler/TablerError404';
 import useTranslation from '@/app/libs/i18n/client';
 
-export default function NotFound() {
-  const {t} = useTranslation(undefined, 'not-found');
+export interface NotFoundCardProps {
+  lng?: string;
+}
+
+export default function NotFoundCard(props: NotFoundCardProps) {
+  const {t} = useTranslation(props.lng, 'components/data/display/NotFoundCard');
   return (
     <div className="card bg-base-100 shadow-lg">
       <div className="card-body items-center text-center">
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NotFoundCard from '@/app/components/data/display/NotFoundCard';
 import CenterMain from '@/app/components/layout/CenterMain';
 
-export default async function NotFound() {
+export default async function RootNotFound() {
   return (
     <html>
       <body>
