0efc0c8d50b7ff47f3d78629fb3a00e45da41332
zer0 <osom8979@gmail.com>
2024-01-22 17:05:37 +0900

    Move locales dir: app/libs/i18n/locales -> app/locales

--- app/libs/i18n/backend.ts
+++ app/libs/i18n/backend.ts
@@ -1,7 +1,7 @@
 import resourcesToBackend from 'i18next-resources-to-backend';
 
 function backendI18nImporter(language: string, namespace: string) {
-  return import(`./locales/${language}/${namespace}.json`);
+  return import(`@/app/locales/${language}/${namespace}.json`);
 }
 
 export const backendJsonModule = resourcesToBackend(backendI18nImporter);
