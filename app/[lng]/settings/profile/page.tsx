afaee08f782186fe241b704e5c62737f58c23574
zer0 <osom8979@gmail.com>
2024-01-25 09:50:43 +0900

    Fix incorrectly matched text colors

--- app/[lng]/settings/profile/page.tsx
+++ app/[lng]/settings/profile/page.tsx
@@ -19,9 +19,7 @@ export default async function SettingsProfilePage(props: I18nRouterProps) {
 
   return (
     <section className="pt-4 sm:pt-0 pr-4">
-      <div className="space-y-0.5 pl-0.5">
-        <h2 className="font-medium text-2xl text-neutral-content">{t('title')}</h2>
-      </div>
+      <h2 className="font-medium text-2xl text-base-content">{t('title')}</h2>
 
       <div className="divider my-2" />
 
@@ -33,13 +31,13 @@ export default async function SettingsProfilePage(props: I18nRouterProps) {
           <div className="card-body space-y-4">
             <label className="flex flex-col w-full space-y-1">
               <div className="label pl-0.5 py-0">
-                <span className="label-text text-neutral-content">
+                <span className="label-text text-base-content">
                   {t('user.nickname.label')}
                 </span>
               </div>
               <input type="text" className="input input-sm input-bordered w-full" />
               <div className="label pl-0.5 py-0">
-                <span className="label-text-alt text-neutral-content/70">
+                <span className="label-text-alt text-base-content/70">
                   {t('user.nickname.details')}
                 </span>
               </div>
