9aa7fb4c448752088e745617e12e1d065d83d098
zer0 <osom8979@gmail.com>
2024-01-30 23:47:56 +0900

    Redesign progress bar page

--- app/[lng]/progress/[code]/page.tsx
+++ app/[lng]/progress/[code]/page.tsx
@@ -1,58 +1,64 @@
 import Link from 'next/link';
 import {type ProgressPageProps} from '@/app/[lng]/progress/[code]/params';
 import CenterLayout from '@/app/components/layout/CenterLayout';
+import SvgNumber from '@/app/components/SvgNumber';
 import SvgSpinners3DotsFade from '@/app/icons/spinners/SvgSpinners3DotsFade';
+import TdesignPercent from '@/app/icons/tdesign/TdesignPercent';
 import useTranslation from '@/app/libs/i18n/server';
-import {appPaths} from '@/app/paths';
 
 export default async function ProgressPage(props: ProgressPageProps) {
-  const {lng, code} = props.params;
+  const {lng} = props.params;
   const {t} = await useTranslation(lng, 'progress');
   const progress = 50;
 
   return (
     <CenterLayout lng={lng}>
-      <section className="osom-card bg-base-200/30">
+      <section className="osom-card rounded-3xl bg-base-200 bg-gradient-to-b shadow-md shadow-base-content/10">
         <div className="card-body items-center space-y-4">
-          <div className="card-title text-center">
-            <h2>{t('title')}</h2>
-          </div>
+          <div className="flex flex-col space-y-2">
+            <div className="flex justify-center items-end w-full space-x-[-0.6rem]">
+              <SvgNumber value={50} />
+              <TdesignPercent className="w-7 h-7 mb-2" />
+            </div>
 
-          <div className="card-title">
-            <h2 className="font-extrabold text-8xl tracking-tighter">
-              {progress}
-              <span className="text-3xl">%</span>
-            </h2>
+            <p className="flex flex-row items-center text-center animate-pulse">
+              <span>{t('progressing')}</span>
+              <span className="ml-1 self-end">
+                <SvgSpinners3DotsFade />
+              </span>
+            </p>
           </div>
 
           <div className="w-full">
             <progress
-              className="progress progress-primary w-full"
+              className="progress h-3 w-full"
               value={progress}
               max="100"
             ></progress>
           </div>
 
-          <p className="flex flex-row items-center text-center animate-pulse">
-            <span>{t('progressing')}</span>
-            <span className="ml-1 self-end">
-              <SvgSpinners3DotsFade />
-            </span>
-          </p>
-
           <div className="card-actions gap-4">
-            <button className="btn btn-md btn-primary w-full sm:w-36">
-              <Link href={`/${lng}${appPaths.progress}/${code}`} hrefLang={lng}>
-                {t('complete')}
+            <button className="btn btn-sm btn-secondary rounded-3xl w-full sm:w-36">
+              <Link href="#" hrefLang={lng}>
+                <p>{t('copy')}</p>
               </Link>
             </button>
 
-            <button className="btn btn-md btn-error btn-outline w-full sm:w-36">
+            <button className="btn btn-sm btn-error btn-outline w-full sm:w-36">
               <Link href="#" hrefLang={lng}>
                 <p>{t('abort')}</p>
               </Link>
             </button>
           </div>
+
+          <p>
+            <small>
+              {t('more_info')}
+              <Link href="#" hrefLang={lng} className="link link-primary ml-1">
+                {t('document_link')}
+              </Link>
+            </small>
+          </p>
         </div>
       </section>
     </CenterLayout>
--- app/components/SvgNumber.tsx
+++ app/components/SvgNumber.tsx
@@ -1,4 +1,4 @@
-import {type HTMLAttributes, useMemo} from 'react';
+import {type HTMLAttributes, SVGProps, useMemo} from 'react';
 import {UnsupportedError} from '@/app/exceptions';
 import IcBaselineMinus from '@/app/icons/ic/IcBaselineMinus';
 import IcBaselinePlus from '@/app/icons/ic/IcBaselinePlus';
@@ -14,54 +14,70 @@ import TdesignNumbers7 from '@/app/icons/tdesign/TdesignNumbers7';
 import TdesignNumbers8 from '@/app/icons/tdesign/TdesignNumbers8';
 import TdesignNumbers9 from '@/app/icons/tdesign/TdesignNumbers9';
 
-interface SvgNumberProps extends HTMLAttributes<HTMLDivElement> {
+interface SvgNumberProps extends Omit<HTMLAttributes<HTMLDivElement>, 'className'> {
   value: number | string;
+  className?: string;
+  svgClassName?: string;
 }
 
-function SvgChar({c}: {c: string}) {
+interface SvgCharProps extends SVGProps<SVGSVGElement> {
+  c: string;
+}
+
+function SvgChar(props: SvgCharProps) {
+  const {c, ...attrs} = props;
   switch (c) {
     case '0':
-      return <TdesignNumbers0 />;
+      return <TdesignNumbers0 {...attrs} />;
     case '1':
-      return <TdesignNumbers1 />;
+      return <TdesignNumbers1 {...attrs} />;
     case '2':
-      return <TdesignNumbers2 />;
+      return <TdesignNumbers2 {...attrs} />;
     case '3':
-      return <TdesignNumbers3 />;
+      return <TdesignNumbers3 {...attrs} />;
     case '4':
-      return <TdesignNumbers4 />;
+      return <TdesignNumbers4 {...attrs} />;
     case '5':
-      return <TdesignNumbers5 />;
+      return <TdesignNumbers5 {...attrs} />;
     case '6':
-      return <TdesignNumbers6 />;
+      return <TdesignNumbers6 {...attrs} />;
     case '7':
-      return <TdesignNumbers7 />;
+      return <TdesignNumbers7 {...attrs} />;
     case '8':
-      return <TdesignNumbers8 />;
+      return <TdesignNumbers8 {...attrs} />;
     case '9':
-      return <TdesignNumbers9 />;
+      return <TdesignNumbers9 {...attrs} />;
     case '.':
-      return <PhDotOutlineFill />;
+      return <PhDotOutlineFill {...attrs} />;
     case '+':
-      return <IcBaselinePlus />;
+      return <IcBaselinePlus {...attrs} />;
     case '-':
-      return <IcBaselineMinus />;
+      return <IcBaselineMinus {...attrs} />;
     default:
       throw new UnsupportedError(`Unsupported SVG character: ${c}`);
   }
 }
 
 export default function SvgNumber(props: SvgNumberProps) {
-  const {value, ...attrs} = props;
+  const {value, className, svgClassName, ...attrs} = props;
   const chars = useMemo(() => {
     const text = typeof value !== 'string' ? value.toString() : value;
     return text.split('');
   }, [value]);
+
+  const finalClassName = ['flex', 'flex-row', 'space-x-[-1.4rem]', className]
+    .filter(v => typeof v !== 'undefined')
+    .join(' ');
+
+  const finalSvgClassName = ['w-16', 'h-16', svgClassName]
+    .filter(v => typeof v !== 'undefined')
+    .join(' ');
+
   return (
-    <div {...attrs}>
+    <div className={finalClassName} {...attrs}>
       {chars &&
         chars.map((c, i) => {
-          return <SvgChar key={i} c={c} />;
+          return <SvgChar key={i} c={c} className={finalSvgClassName} />;
         })}
     </div>
   );
